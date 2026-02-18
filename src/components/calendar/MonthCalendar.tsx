c86fb2a6f0e23732eda6bc22aa957ab8be204cb9
efpear <ivan.canariasfp@gmail.com>
2026-02-18 16:34:30 +0000

    fix: strict-mode fixes for MonthCalendar.tsx

--- src/components/calendar/MonthCalendar.tsx
+++ src/components/calendar/MonthCalendar.tsx
@@ -3,7 +3,6 @@
  * Grid mensual de calendario
  */
 
-import React from 'react';
 import type { FechaISO, ModuloConSesiones } from '@/types';
 import { obtenerDiaSemana, primerDiaMes, restarDias, sumarDias, MESES_ES, DIAS_ES } from '@/utils/date-utils';
 import { DayCell } from './DayCell';
@@ -25,9 +24,9 @@ export function MonthCalendar({ anio, mes, modulos, festivos, onDayClick, fechaS
   const diasMes: FechaISO[] = [];
   let actual = primerDia;
   while (new Date(actual).getMonth() + 1 === mes) { diasMes.push(actual); actual = sumarDias(actual, 1); }
-  const ultimoDiaSemana = obtenerDiaSemana(diasMes[diasMes.length - 1]);
+  const ultimoDiaSemana = obtenerDiaSemana(diasMes[diasMes.length - 1]!);
   const diasSiguientes: FechaISO[] = [];
-  for (let i = 1; i < 7 - ultimoDiaSemana; i++) diasSiguientes.push(sumarDias(diasMes[diasMes.length - 1], i));
+  for (let i = 1; i < 7 - ultimoDiaSemana; i++) diasSiguientes.push(sumarDias(diasMes[diasMes.length - 1]!, i));
   const todosLosDias = [...diasAnteriores, ...diasMes, ...diasSiguientes];
   const semanas: FechaISO[][] = [];
   for (let i = 0; i < todosLosDias.length; i += 7) semanas.push(todosLosDias.slice(i, i + 7));
